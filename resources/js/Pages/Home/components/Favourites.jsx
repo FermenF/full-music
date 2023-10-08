3e7fd8c32a4fed709bf11d30389a6fa3db32a003
Fermen1305 <fermen1305@gmail.com>
2023-10-08 15:40:39 -0500

    Dark mode bug

--- resources/js/Pages/Home/components/Favourites.jsx
+++ resources/js/Pages/Home/components/Favourites.jsx
@@ -17,15 +17,15 @@ const Favourites = () => {
                     <div className="text-left flex">
                         <button className="text-white font-bold bg-transparent border-2 border-white rounded-3xl p-2 hover:bg-slate-400" style={{ letterSpacing: '1px' }}>Favourites</button>
                         <button className="flex items-center text-white bg-transparent border-2 border-white rounded-3xl lg:p-2 md:p-1 p-2 hover:bg-slate-400">
-                            <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
+                            <svg className="w-6 h-6 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
                                 <path d="M17.947 2.053a5.209 5.209 0 0 0-3.793-1.53A6.414 6.414 0 0 0 10 2.311 6.482 6.482 0 0 0 5.824.5a5.2 5.2 0 0 0-3.8 1.521c-1.915 1.916-2.315 5.392.625 8.333l7 7a.5.5 0 0 0 .708 0l7-7a6.6 6.6 0 0 0 2.123-4.508 5.179 5.179 0 0 0-1.533-3.793Z"/>
                             </svg>
                         </button>
                     </div>
                 </div>
                 <div className="text-right">
-                    <button className="text-black bg-white lg:p-6 p-3 md:p-4 rounded-full hover:bg-slate-400">
-                        <svg className="lg:w-6 lg:h-6 md:w-3 md:h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                    <button className="text-black bg-white p-3 md:p-6 rounded-full hover:bg-slate-800">
+                        <svg className="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
                         </svg>
                     </button>
