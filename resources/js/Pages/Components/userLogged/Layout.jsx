5d0dacb0e352cebee393ba20f54334604e3f30c8
Fermen1305 <fermen1305@gmail.com>
2023-10-09 12:34:58 -0500

    Responsive all dashboard

--- resources/js/Pages/Components/userLogged/Layout.jsx
+++ resources/js/Pages/Components/userLogged/Layout.jsx
@@ -7,11 +7,11 @@ const Layout = ({ children }) => {
     return(
         <div className="flex flex-col min-h-screen overflow-hidden bg-gray-300 p-1">
             <div className="grid grid-cols-1 md:flex-grow md:grid-cols-12 md:grid-rows-2 md:gap-1">
-                <div className="overflow-hidden rounded-md md:row-span-2 md:col-span-2">
+                <div className="overflow-hidden rounded-md md:row-span-2 md:col-span-4 lg:col-span-3 xl:col-span-2">
                     <NavBarAndSearch/>
                     <Library />
                 </div>
-                <div className="md:flex-grow w-full h-full col-span-10 row-span-2 rounded-md bg-slate-100">
+                <div className="md:flex-grow w-full h-full md:col-span-8 lg:col-span-9 row-span-2 xl:col-span-10 rounded-md bg-slate-100">
                     <div className="hidden md:block">
                         <Header/>
                     </div>
