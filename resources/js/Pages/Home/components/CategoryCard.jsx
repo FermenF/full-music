ec37f22d58188246f52f83aa33b7452ff4e27f6f
Fermen1305 <fermen1305@gmail.com>
2023-10-11 14:35:28 -0500

    click categories bug solved

--- resources/js/Pages/Home/components/CategoryCard.jsx
+++ resources/js/Pages/Home/components/CategoryCard.jsx
@@ -4,14 +4,14 @@ import React from "react";
 const CategoryCard = ({ category }) => {
     const { id, name, picture_medium } = category;
     return (
-        <figure className="relative max-w-sm transition-all duration-300 cursor-pointer filter hover:grayscale">
-            <Link href={`/genre/${ id }/${ name.replace('/', '_&_') }/artists`}>
-                <img className="rounded-lg" src={ picture_medium } alt="image description" />
-            </Link>
-            <figcaption className="absolute px-4 text-lg text-white bottom-6">
-                <strong className="break-words">{ name.replace('/', ' / ') }</strong>
-            </figcaption>
-        </figure>
+        <Link href={`/genre/${ id }/${ name.replace('/', '_&_') }/artists`}>
+            <figure className="relative max-w-sm transition-all duration-300 cursor-pointer filter hover:grayscale">
+                    <img className="rounded-lg" src={ picture_medium } alt="image description" />
+                <figcaption className="absolute px-4 text-lg text-white bottom-6">
+                    <strong className="break-words">{ name.replace('/', ' / ') }</strong>
+                </figcaption>
+            </figure>
+        </Link>
     );
 }
 
