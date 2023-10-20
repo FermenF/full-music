0a427286c10ab0b913e41f606525ed8ba89bc72d
Fermen1305 <fermen1305@gmail.com>
2023-10-20 13:38:30 -0500

    play styles

--- resources/js/Pages/Dashboard/components/PlayBanner.jsx
+++ resources/js/Pages/Dashboard/components/PlayBanner.jsx
@@ -1,25 +1,105 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { SongContext } from "../../Context/SongContext";
 
 const PlayBanner = () => {
-    const { song } = useContext( SongContext );
+    const { song } = useContext(SongContext);
 
-    if(song){
-        return (
-            <div id="bottom-banner" tabIndex="-1" className="fixed bottom-0 left-0 z-50 flex justify-between w-full p-4 border-t border-gray-200 bg-gray-50 dark:bg-gray-700 dark:border-gray-600">
-                <audio src={ song } controls="controls" preload="none"></audio>
-                <div className="flex items-center">
-                    <button data-dismiss-target="#bottom-banner" type="button" className="flex-shrink-0 inline-flex justify-center w-7 h-7 items-center text-gray-400 hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 dark:hover:bg-gray-600 dark:hover:text-white">
-                        <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
-                        </svg>
-                        <span className="sr-only">Close banner</span>
-                    </button>
+    const [audio] = useState(new Audio(song));
+    const [isPlaying, setIsPlaying] = useState(true);
+
+    const [duration, setDuration] = useState(0);
+    const [currentTime, setCurrentTime] = useState(0);
+
+    const handlePlayPause = () => {
+        if (isPlaying) {
+            audio.pause();
+        } else {
+            audio.play();
+        }
+        setIsPlaying(!isPlaying);
+    };
+
+    useEffect(() => {
+        audio.src = song;
+        if (isPlaying) {
+            audio.play();
+        }
+        setIsPlaying(true);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [song]);
+
+    const formatTime = (time) => {
+        const minutes = Math.floor(time / 60);
+        const seconds = Math.floor(time % 60);
+        return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+    };
+
+    return (
+        <div id="bottom-banner" tabIndex="-1" className="fixed text-white bottom-0 left-0 z-50 flex justify-between items-center w-full p-2 px-5 rounded-md bg-slate-800">
+            <div className="flex items-center">
+                <img src="https://lastfm.freetls.fastly.net/i/u/300x300/a45d1688f196028c8e0f31b357e748a4.png" className="img-fluid h-16 w-16 rounded-md" />
+                <div className="ml-3">
+                    <div className="font-bold text-sm">
+                        <h1>Danza Rota Remasterizado 2007</h1>
+                    </div>
+                    <div className="text-gray-500 font-extralight text-xs">
+                        <h5>Soda Stereo</h5>
+                    </div>
+                </div>
+                <div className="ml-3">
+                    <svg className="w-5 h-5 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 21 19">
+                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11 4C5.5-1.5-1.5 5.5 4 11l7 7 7-7c5.458-5.458-1.542-12.458-7-7Z" />
+                    </svg>
                 </div>
             </div>
-        );
-    }
-
+            <div className="w-96">
+                <div className="flex items-center justify-center my-2">
+                    <button className="mx-1 rounded-full p-2 hover:bg-black">
+                        <svg className="w-4 h-4 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 14">
+                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11 10H1m0 0 3-3m-3 3 3 3m1-9h10m0 0-3 3m3-3-3-3" />
+                        </svg>
+                    </button>
+                    <button className="mx-1 rounded-full p-2 hover:bg-black">
+                        <svg className="w-4 h-4 text-white pr-0.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 1 1 5l4 4" />
+                        </svg>
+                    </button>
+                    <button
+                        className="mx-1 rounded-full p-2 text-black bg-white hover:bg-gray-600 hover:text-white"
+                        onClick={handlePlayPause}
+                    >
+                        {isPlaying ? (
+                            <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 12 16">
+                                <path d="M3 0H2a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h1a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm7 0H9a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h1a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Z" />
+                            </svg>
+                        ) : (
+                            <svg className="w-4 h-4 pl-0.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 10 16">
+                                <path d="M3.414 1A2 2 0 0 0 0 2.414v11.172A2 2 0 0 0 3.414 15L9 9.414a2 2 0 0 0 0-2.828L3.414 1Z" />
+                            </svg>
+                        )}
+                    </button>
+                    <button className="mx-1 rounded-full p-2 hover:bg-black">
+                        <svg className="w-4 h-4 text-white pl-0.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 9 4-4-4-4" />
+                        </svg>
+                    </button>
+                    <button className="mx-1 rounded-full p-2 hover:bg-black">
+                        <svg className="w-4 h-4 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
+                            <path d="M17 9a1 1 0 0 0-1 1 6.994 6.994 0 0 1-11.89 5H7a1 1 0 0 0 0-2H2.236a1 1 0 0 0-.585.07c-.019.007-.037.011-.055.018-.018.007-.028.006-.04.014-.028.015-.044.042-.069.06A.984.984 0 0 0 1 14v5a1 1 0 1 0 2 0v-2.32A8.977 8.977 0 0 0 18 10a1 1 0 0 0-1-1ZM2 10a6.994 6.994 0 0 1 11.89-5H11a1 1 0 0 0 0 2h4.768a.992.992 0 0 0 .581-.07c.019-.007.037-.011.055-.018.018-.007.027-.006.04-.014.028-.015.044-.042.07-.06A.985.985 0 0 0 17 6V1a1 1 0 1 0-2 0v2.32A8.977 8.977 0 0 0 0 10a1 1 0 1 0 2 0Z" />
+                        </svg>
+                    </button>
+                </div>
+                <div className="flex">
+                    <small className="mx-1 text-gray-600 font-extralight text-sm">0:00</small>
+                    <input type="range" className="w-full" />
+                    <small className="mx-1 text-gray-600 font-extralight text-sm">5:00</small>
+                </div>
+            </div>
+            <div className="">
+                <p>ALGUNOS BOTONES DE ACCIÓN</p>
+            </div>
+        </div>
+    );
 };
 
 export default PlayBanner;
